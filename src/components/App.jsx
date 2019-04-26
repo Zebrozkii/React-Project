ddd73143b6dd2ec6de8bc7c5fe5c236d1d7c9443
Jimmy Zebroski <zebrozkii@gmail.com>
2019-04-26 08:41:26 -0700

    created file structure everything works

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+
+function App(){
+  return (
+    <div>
+    <h1>Jimmy</h1>
+    </div>
+  );
+}
+
+export default App;
