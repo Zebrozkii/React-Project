3d0f3c1835c641e2d364cd527798ef34fd9fe79a
Jimmy Zebroski <zebrozkii@gmail.com>
2019-04-29 15:45:08 -0700

    added picture and hard coded stuff to one page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import surprise from '../assets/images/image1-2.jpeg';
 
 
 
@@ -8,9 +10,17 @@ function About(){
 
     <div>
     <style jsx>{`
+      img{
+        transform: rotate(270deg);
+        position:absolute;
+        top: 80px;
+        right: -150px;
+      }
       `}</style>
       <div>
-      <h1>Making sure route works</h1>
+      <h1>About Me</h1><Link to="/"><h3>Home</h3></Link><img src={surprise}/>
+      <p>Hello, i got my first taste of programming back in a intro class to C++ and since then ive loved it.
+      I thouroughly enjoy the struggle of solving problems with the tools i know and leraning new thing constatnly always keeps me entertained</p>
     </div>
     </div>
   );
