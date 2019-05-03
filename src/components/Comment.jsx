9e2d3642a45932bba2680fc5f0bbfad3fbb56045
Jimmy Zebroski <zebrozkii@gmail.com>
2019-05-03 08:17:01 -0700

    restarted setting up state cause im using it in a different way then in the lessons

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,45 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import surprise from '../assets/images/image1-2.jpeg';
-import NewCommentForm from './NewCommentForm';
-import PropTypes from "prop-types";
+
 
 class Comment extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-      masterCommentList:[]
-    };
-    this.handleAddingNewComment= this.handleAddingNewComment.bind(this);
-    this.handleTroubleshootingConfirmation= this.handleTroubleshootingConfirmation.bind(this);
-  }
-  handleAddingNewComment(newComment){
-    var newMasterCommentList = this.state.masterCommentList.slice();
-    newMasterCommentList.push(newComment);
-    this.setState({masterCommentList: newMasterCommentList});
-  }
-  handleTroubleshootingConfirmation(){
-    this.setState({formVisableOnPage:true});
-  }
   render(){
-    let currentlyVisibleContent = null;
-    if(this.state.formVisibleOnPage){
-      currentlyVisibleContent = <NewCommentForm onNewCommentCreation={this.props.onNewCommentCreation}/>;
-    }else{
-      <p>sfdkjfaiusebnfijwaenfnibnerfibeibiabfsjidbnfjsbdvbnsidbv</p>
-    }
-    return (
-
-    <div>
+  return (
       <div>
-      {currentlyVisibleContent}
-      <NewCommentForm/>
+        <h1>Let Me know you stopped by:)</h1>
+        <form>
+        <input
+        type='text'
+        id = 'name'
+        placeholder = 'userName'/>
+        <textarea
+        id = 'comment'
+        placeholder = 'userComment'/>
+        <button type='submit'>Comment</button>
+        </form>
       </div>
-    </div>
   );
 }
 }
-Comment.propTypes={
-  onNewCommentCreation: PropTypes.func
-};
+
 export default Comment;
