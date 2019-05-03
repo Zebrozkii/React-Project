4fe12a5664606ecd538c860114f1c7a0e8330b3c
Jimmy Zebroski <zebrozkii@gmail.com>
2019-05-03 08:50:32 -0700

    i dont know whats going on so im gonna take a break and watch some react vids on state

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,20 +3,11 @@ import { Link } from 'react-router-dom';
 
 
 class Comment extends React.Component{
+
   render(){
   return (
       <div>
-        <h1>Let Me know you stopped by:)</h1>
-        <form>
-        <input
-        type='text'
-        id = 'name'
-        placeholder = 'userName'/>
-        <textarea
-        id = 'comment'
-        placeholder = 'userComment'/>
-        <button type='submit'>Comment</button>
-        </form>
+      <p>Wtf</p>
       </div>
   );
 }
