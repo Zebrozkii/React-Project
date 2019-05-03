09968da08facaa3a33f561b770353d3f2832f1e9
Jimmy Zebroski <zebrozkii@gmail.com>
2019-05-03 12:15:11 -0700

    ok after console logging and googleing ive got the right input but am still struggling with the output

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 
 class Comment extends React.Component{
 
   constructor(props){
     super(props);
-    this.state = { comment: '' };
+    this.state = { };
+    this.handleChange = this.handleChange.bind(this);
   }
   handleChange(e){
-    event.preventDefault();
-    this.setState({ comment : commen});
-
+    this.setState({ comment: event.target.value});
   };
 
   render(){
   return (
     <div>
     <form>
-    <label htmlFor="comment">Comment </label>
     <input
     type="text"
     name="comment"
+    id="comment"
     value={this.state.comment}
     onChange={this.handleChange}
     />
