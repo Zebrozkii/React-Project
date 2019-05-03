4e1f2c3b0fcacb067810b5a361b6327d3f056446
Jimmy Zebroski <zebrozkii@gmail.com>
2019-05-03 11:59:35 -0700

    still struggling with state

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
 
 class Comment extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state = { comment: '' };
+  }
+  handleChange(e){
+    event.preventDefault();
+    this.setState({ comment : commen});
+
+  };
+
   render(){
   return (
-      <div>
-      <p>Wtf</p>
-      </div>
+    <div>
+    <form>
+    <label htmlFor="comment">Comment </label>
+    <input
+    type="text"
+    name="comment"
+    value={this.state.comment}
+    onChange={this.handleChange}
+    />
+    </form>
+    <h3>Comment Left: {this.state.comment}</h3>
+    </div>
   );
 }
 }
--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 
-function NewCommentForm(){
-
-  return (
-  );
-}
-
-export default NewCommentForm;
